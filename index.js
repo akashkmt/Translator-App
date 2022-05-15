ed32e408f923bc20b4ced38dc897cecaddeef59c
akashkmt <akashkmt963@gmail.com>
2022-05-15 14:53:28 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+function translateFunction(){
+    let enteredText = document.getElementById('inputText').value;
+    let inputLanguage = document.getElementById('inputLanguage').value;
+    let outputLanguage = document.getElementById('outputLanguage').value;
+
+    // console.log(enteredText, inputLanguage, outputLanguage);
+    if(enteredText==''){
+        alert('Please Enter Input');
+    }
+    else{
+        translateText(enteredText, inputLanguage, outputLanguage);
+    }
+}
+
+async function translateText(enteredText, inputLanguage, outputLanguage){
+    try{
+        let result = await fetch("https://libretranslate.de/translate", {
+            method: "POST",
+            body: JSON.stringify({
+                q: enteredText,
+                source: inputLanguage,
+                target: outputLanguage,
+                format: "text"
+            }),
+            headers: { "Content-Type": "application/json" }
+        });
+
+        let output = await result.json();
+        // console.log(output);
+        let convertedText = output.translatedText;
+        showOutput(convertedText);
+    }
+    catch(error){
+        console.log(error)
+    }
+}
+
+function showOutput(convertedText){
+    document.getElementById('outputText').value = convertedText;
+}
